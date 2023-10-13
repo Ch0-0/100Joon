b098a72744e8ba52907e7d54b22b08c87a989fad
CHANYEONG KIM <96282369+Ch0-0@users.noreply.github.com>
2023-10-13 16:47:50 +0900

    Update 팩맨-임시.java

--- 팩맨-임시.java
+++ 팩맨-임시.java
@@ -1,5 +1,5 @@
-java.util.*;
-java.io.*;
+import java.util.*;
+import java.io.*;
 
 public class test{
   public static int[][] board = new int[4][4];
@@ -28,23 +28,24 @@ public class test{
     public Monster(int x, int y){
       this.x = x;
       this.y = y;
+    }
       public boolean isSame(Monster m){
-        return this.x = m.x && this.y = m.y;
+        return this.x == m.x && this.y == m.y;
       }
   }
 
-  public static void(boolean eggborn){
+  public static void copy(boolean eggborn){
     //알이 부화하면서 몬스터보드에 추가된다.
     if(eggborn){
       for(int i=0; i<4; i++){
-        for(int j=0; j<4; j++{
+        for(int j=0; j<4; j++){
           board[i][j] += egg[i][j];  
+          egg[i][j] = 0;
         }
       }
-     egg = new int[4][4];
     } else { //몬스터가 알을 낳는다.
       for(int i=0; i<4; i++){
-        for(int j=0; j<4; j++{
+        for(int j=0; j<4; j++){
           egg[i][j] += board[i][j];  
         }
       }
@@ -52,71 +53,120 @@ public class test{
   }
 
   public static boolean canGo(int x, int y){
-    return x>0 && y>0 && x<5 && y<5;
+    return x>=0 && y>=0 && x<4 && y<4;
   }
 
   public static int PackManMove(int x, int y){
     int max = -1;
-    Queue p<int[]> = new LinkedList<>();
-    p.add(new int[] {x,y,0,0});
+    Queue<int[]> p = new LinkedList<>();
+    p.add(new int[] {x,y,0,0,-1,-1,-1});
     
-    while(!isEmpty()){
+    while(!p.isEmpty()){
       int[] pp = p.poll();
       int pdx = pp[0];
       int pdy = pp[1];
       int eat = pp[2];
       int cnt = pp[3];
+      int m1 = pp[4];
+      int m2 = pp[5];
+      int m3 = pp[6];
+      int b1 = 0;
+      int b2 = 0;
+      int b3 = 0;
 //큐에 각 이동이력 남기기
+     
       if(cnt == 3) {
           if(max<eat){
             max = eat;
-            pmx = rx;
-            pmy = ry;
+            pmx = pdx;
+            pmy = pdy;
+            b1 = m1;
+            b2 = m2;
+            b3 = m3;
           }
         continue;
       }
       
-
+cnt ++;
       for(int i=0; i<4; i++){
         int rx = pdx + dx[i];
         int ry = pdy + dy[i];
-        if(conGo(rx,ry)){
-          eat += board[rx][ry];
-          cnt ++;
-          p.add(new int[] {rx,ry,eat,cnt});
+        if(canGo(rx,ry)){
+           switch(cnt){
+             case 1: p.add(new int[] {rx,ry,eat+board[rx][ry],cnt,i,-1,-1});
+             break;
+             case 2: p.add(new int[] {rx,ry,eat+board[rx][ry],cnt,m1,i,-1});
+             break;
+             case 3: p.add(new int[] {rx,ry,eat+board[rx][ry],cnt,m1,m2,i});
+             break;
+             default:
+               break;
+      }
+          
         } 
       }
-      
+
+        
     }
 
-    
-    
+      int[] tmx = new int[] {1,0,-1,0};
+      int[] tmy = new int[] {0,1,0,-1};
+      board[pmx][pmy] = 0;
+      dead[pmx][pmy] = 2;
+      if(b3 != -1){
+        int tx = pmx + tmx[b3];
+        int ty = pmy + tmy[b3];
+        board[tx][ty] = 0;
+        dead[tx][ty] = 2;
+        board[tx+tmx[b2]][ty+tmy[b2]] = 0;
+        dead[tx+tmx[b2]][ty+tmy[b2]] = 2;
+        
+      }
+    mCnt = mCnt - max;
     return max;
   }
 
   public static void MonsterMove(){
-
-    while(!q.isEmpty()){
+    int mplus = 0;
+    for(int mrepeat=0; mrepeat<mCnt; mrepeat++){
       int[] qp = q.poll();
       int mdx = qp[0];
       int mdy = qp[1];
       int mddir = qp[2];
-
+      q.add(new int[] {mdx,mdy,mddir,0});
+      mplus++;
+      
       for(int i=0; i<8; i++){
         int rmddir = (mddir + i + 8) % 8;
         int rmdx = mdx + ddx[rmddir];
         int rmdy = mdy + ddy[rmddir];
         if(canGo(rmdx,rmdy)){
-          board[mdx][mdy] --;
-          board[rmdx][rmdy] ++;
-          q.add[
-          break;
+          if(dead[rmdx][rmdy] == 0){
+              if(!(rmdx == pmx && rmdy == pmy)){
+              board[mdx][mdy] --;
+              board[rmdx][rmdy] ++;
+              q.add(new int[] {rmdx,rmdy,i,});
+              mplus++;
+              break;
+            }
+          }
+        } else if(mdx==rmdx && mdy==rmdy){
+          q.add(new int[] {rmdx,rmdy,mddir});
+          mplus++;
         }
       }
 
     }
-
+  mCnt += mplus;
   }
+
+ public static void deadminus() {
+  for(int i=0; i<4; i++){
+    for(int j=0; j<4; j++){
+       dead[i][j]--; 
+    }
+  }
+ }
   
   public static void main(String[] args) throws Exception{
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -131,18 +181,26 @@ public class test{
 
     for(int i=0; i<mCnt; i++){
       st = new StringTokenizer(br.readLine());
-      int mx = Integer.parseInt(st.nextToken());
-      int my = Integer.parseInt(st.nextToken());
-      int mdir = Integer.parseInt(st.nextToken());
+      int mx = Integer.parseInt(st.nextToken()) -1;
+      int my = Integer.parseInt(st.nextToken()) -1;
+      int mdir = Integer.parseInt(st.nextToken()) -1;
       q.add(new int[] {mx,my,mdir,0});
       board[mx][my]++;
     }
-
+    for(int i=0; i<repeat; i++){
+      copy(false);
+      MonsterMove();
+      PackManMove(pmx,pmy);
+      deadminus();
+      copy(true);
+    }
+  int result = 0;
     for(int i=0; i<4; i++){
-      for(int j=0; j<4; j++{
-          
+      for(int j=0; j<4; j++){
+        result += board[i][j];
       }
     }
-
+System.out.println(result);
+  }
 
 }
